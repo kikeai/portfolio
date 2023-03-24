4528fed864784649a4ecc423471a1911d738d74b
kikeai <lg5967133@gmail.com>
2023-03-24 15:12:23 -0400

    change colors buttons

--- src/layouts/modal/ButtonModal.tsx
+++ src/layouts/modal/ButtonModal.tsx
@@ -12,7 +12,7 @@ const ButtonModal = ({onClick, text,bgColor,borderColor,textColor, icon}: Props)
         <button
         onClick={onClick}
         className={`bg-${borderColor} rounded-lg transition-all w-full  duration-200 hover:cursor-pointer`}>
-            <p className={`flex justify-center items-center bg-${bgColor} text-${textColor} text-center rounded-md border-2 border-${borderColor}  py-2 px-6 2xl:py-3 2xl:px-7 translate-x-[-0.25em] translate-y-[-0.25em] transition-all duration-200 text-lg 2xl:text-2xl font-montserrat font-semibold active:translate-x-[0] active:translate-y-[0]`}>
+            <p className={`flex justify-center items-center ${bgColor === "whatsapp"? "bg-whatsapp" : bgColor === "linkedin"? "bg-linkedin": bgColor === "light"? "bg-light": "bg-dark"} text-${textColor} text-center rounded-md border-2 border-${borderColor}  py-2 px-6 2xl:py-3 2xl:px-7 translate-x-[-0.25em] translate-y-[-0.25em] transition-all duration-200 text-lg 2xl:text-2xl font-montserrat font-semibold active:translate-x-[0] active:translate-y-[0]`}>
                 <img className="mr-3 h-6" src={icon} alt="icon" />{text}
             </p>
         </button>
